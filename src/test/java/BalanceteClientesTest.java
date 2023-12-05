b74187dc0a459173bc3b1f8cd34c396caca33206
Juarez Campos <42527050+JuarezCampos93@users.noreply.github.com>
2023-12-05 08:37:07 -0300

    Update BalanceteClientesTest.java

--- src/test/java/BalanceteClientesTest.java
+++ src/test/java/BalanceteClientesTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BalanceteTest {
+class BalanceteClienteTest {
 
     @Test
     void deveContarClientesPresentesPizzaria() {
